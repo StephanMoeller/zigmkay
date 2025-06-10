027c4087867d853f3f07bc56ef6b9693e666df91
Stephan Moeller <stephanryer@hotmail.com>
2025-06-10 12:06:51 +0200

    added cmp capabilities to the zls config for correct autosuggests

--- src/keycodes.zig
+++ src/keycodes.zig
@@ -1,5 +1,8 @@
-pub const Asome = 0x21;
-pub const BBB = 0x21;
+pub const A = 0x21;
+pub const B = 0x21;
+pub const Aa = 0x21;
+pub const Oe = 0x21;
+pub const Ae = 0x21;
 
 pub fn K(keycode: u8) u8 {
     return keycode;
--- src/main.zig
+++ src/main.zig
@@ -27,28 +27,10 @@ const RGUI: u8 = 0b10000000;
 //  Mod
 //  Momentary layer + mod
 //  => layer: ?u8, => mods: Mods
-
-const KeyDef = struct {
-    HoldMods: u8,
-    HoldMomentaryLayer: u4,
-    TapKeyCode: u8,
-    _apMods: c.Mods,
-    TapOneShotLayer: u4,
-    TapPermanentLayer: u4,
-};
-
 const Foo = packed struct { a: u47, b: u20 };
 
-const HoldDef = union(enum) { LayerChangeMomentarily: c.LayerWithMods, None: void };
-const TapDef = union(enum) { LayerChangeOneShot: c.LayerWithMods, LayerChangePermanent: c.LayerWithMods, FireKeyCode: c.KeyCodeWithMods, None: void };
-const TapHoldDef = struct { tap: TapDef, hold: HoldDef };
-const ComboDef = struct { keyPos1: [2]u8, keyPos2: [2]u8, keyPos3: [2]u8, Action: TapHoldDef };
-const v = .{};
-//const Test: [2][3]u8 = [2][3]u8{[_]u8{1,2,3},[_]u8{4,5,6}};
-const LayerCount = 4;
-const KeyCount = 36;
-
 pub fn main() !void {
-    std.log.info("hey", .{});
+    const k = c.nthisisokay{};
+    std.log.info("hey", .{k});
     std.debug.print("Size is {any}.\n", .{kc.BBB});
 }
