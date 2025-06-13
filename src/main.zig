ec5697a15d68af695aa69954073f8b47229a28ab
Stephan Moeller <stephanryer@hotmail.com>
2025-06-13 15:35:03 +0200

    added todo

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,24 @@ const keyboard = @import("wilson26/keymap.zig").KeyboardConfig;
 const keyboardDef = keyboard.keymap;
 const pinSetup = keyboard.rowPins;
 
+// Init
+// - get input/output pins
+// - get x/y scanning coordinates
+// - get layered keymap
+//
+// Loop
+// - Scan Matrix and compare to last scanned matrix (keep track of last change per position in matrix to implement debounce)
+// - Produces list of matrix scans and add it to the event list
+// - Process event queue by determining if anything can be done at this point, create actions and remove the corresponding events from the event list
+// - Process the given actions (fire keycodes presses/releases and layer changes)
+
+const MatrixEvent = struct { col: usize, row: usize, state: bool };
+fn scanMatrix() []MatrixEvent {
+    unreachable;
+}
+
 pub fn main() !void {
     // init pins
+    scanMatrix();
     std.log.info("keycount: {any}", .{keyboard});
 }
