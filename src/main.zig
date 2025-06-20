4d6190fc1825ecd53298043a500e2c6527b9d0d8
Stephan Moeller <stephanryer@hotmail.com>
2025-06-20 21:46:41 +0200

    worker poc of pressing a key and blinking an led

--- src/main.zig
+++ src/main.zig
@@ -5,30 +5,21 @@ const time = rp2xxx.time;
 
 // Compile-time pin configuration
 const pin_config = rp2xxx.pins.GlobalConfiguration{
-    .GPIO17 = .{
-        .name = "led_red",
-        .direction = .out,
-    },
-    .GPIO16 = .{
-        .name = "led_green",
-        .direction = .out,
-    },
-    .GPIO25 = .{
-        .name = "led_blue",
-        .direction = .out,
-    },
+    .GPIO17 = .{ .name = "led_red", .direction = .out },
+    .GPIO16 = .{ .name = "led_green", .direction = .out },
+    .GPIO25 = .{ .name = "led_blue", .direction = .out },
+    .GPIO29 = .{ .name = "row0", .direction = .out },
+    .GPIO4 = .{ .name = "col0", .direction = .in },
 };
 const pins = pin_config.pins();
 
 pub fn main() !void {
     pin_config.apply();
-
+    pins.row0.put(1);
     while (true) {
-        pins.led_red.toggle();
-        time.sleep_ms(250);
-        pins.led_green.toggle();
-        time.sleep_ms(250);
-        pins.led_blue.toggle();
-        time.sleep_ms(250);
+        const val: u1 = pins.col0.read();
+        pins.led_red.put(val);
+        pins.led_green.put(val);
+        pins.led_blue.put(val);
     }
 }
