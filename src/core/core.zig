c722d38b8b8c5f8947edc557d6b770ffc2892c60
Stephan Moeller <stephanryer@hotmail.com>
2025-06-26 14:47:19 +0200

    work in progress

--- src/core/core.zig
+++ src/core/core.zig
@@ -1,30 +1,28 @@
-pub const Mods = packed struct { ls: bool = false, rs: bool = false, lc: bool = false, rc: bool = false, la: bool = false, ra: bool = false, lg: bool = false, rg: bool = false };
-pub const KeyCodeWithMods = struct { keycode: u8, mods: Mods = Mods{} };
-pub const LayerWithMods = struct { layer: u8, mods: Mods = Mods{} };
-pub const KeyDef = struct {
-    keycode: u8,
-    keycodeMods: Mods = Mods{},
-    mods: Mods = Mods{},
-    //onTap: u8, //function
-    //onHoldEnter: u8,
-    //onHoldExit: u8,
-    layer: u8 = 0x0,
-};
-pub fn PinConfig(comptime colCount: usize, comptime rowCount: usize) type {
-    return struct {
-        colPins: [colCount]u8,
-
-        rowPins: [rowCount]u8,
-    };
+pub const KeyDef = struct { keycode: u8 };
+pub fn TapOnly(keycode: u8) KeyDef {
+    return KeyDef{ .keycode = keycode };
 }
 
-pub fn KeyboardType(comptime layerCount: usize, comptime keyCount: usize, comptime colCount: usize, comptime rowCount: usize) type {
-    return struct {
-        keymap: [layerCount][keyCount]KeyDef,
-        layerCount: usize = layerCount,
-        keyCount: usize = keyCount,
-        pinConfig: PinConfig(colCount, rowCount),
-    };
+pub const Action = union {
+    KeyCodePress: u8, //this should be extended with some sort of mod information
+    KeyCodeRelease: u8, //-,,-
+};
+const KeyIndex = usize;
+pub const InputEvent = union(enum) {
+    key_pressed: KeyIndex,
+    key_released: KeyIndex,
+    pub fn KeyPress(index: KeyIndex) InputEvent {
+        return .{ .key_pressed = index };
+    }
+    pub fn KeyReleased(index: KeyIndex) InputEvent {
+        return .{ .key_released = index };
+    }
+};
+
+pub fn Process(input: []InputEvent) []Action {
+    const actions = [1]Action{Action{ .KeyCodePress = 0x05 }};
+    _ = input;
+    return actions;
 }
 
 // pub const ScanSettings = struct { debounce_ms: u8 };
---
+++ src/core/tests/processing_tapping_only.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const core = @import("../core.zig");
+
+test "tapping - single layer, only tapping defined" {
+    const a = 0x04;
+    const b = 0x05;
+    const c = 0x06;
+    const d = 0x07;
+    const A = core.TapOnly(a);
+    const B = core.TapOnly(b);
+    const C = core.TapOnly(c);
+    const D = core.TapOnly(d);
+
+    const KeyCount = 4;
+    const input_ev = [_]core.InputEvent{.{ .key_pressed = 1 }};
+    const keymap = [_][KeyCount]core.KeyDef{.{ A, B, C, D }};
+
+    const actions = core.Process(input_ev, keymap);
+    try std.testing.expectEqual(actions.len, 1);
+}
