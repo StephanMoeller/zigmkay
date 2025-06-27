886cd863b30d9e393e823036948d7b7738f8b069
Stephan Moeller <stephanryer@hotmail.com>
2025-06-27 13:46:23 +0200

    fixed bugs in generic queue

--- src/generic_queue.zig
+++ src/generic_queue.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
 
 const EnqueueError = error{CapacityExceeded};
+const DequeueError = error{CannotDequeueAmount};
 pub fn GenericQueue(comptime T: type, comptime max_capacity: usize) type {
     return struct {
         const Self = @This();
         data: [max_capacity]T,
-        var counter: usize = 0;
+        counter: usize = 0,
         pub fn Create() Self {
             return Self{ .data = [1]T{undefined} ** max_capacity };
         }
@@ -13,26 +14,30 @@ pub fn GenericQueue(comptime T: type, comptime max_capacity: usize) type {
             return self.counter;
         }
         pub fn enqueue(self: *Self, element: T) EnqueueError!void {
-            if (counter == max_capacity) {
+            if (self.counter == max_capacity) {
                 return EnqueueError.CapacityExceeded;
             }
-            self.data[counter] = element;
-            counter = counter + 1;
+            self.data[self.counter] = element;
+            self.counter = self.counter + 1;
         }
         pub fn read_all_values(self: *Self) []const T {
-            return self.data[0..counter];
+            return self.data[0..self.counter];
         }
-        pub fn dequeue_count(self: *Self, count: usize) void {
-            for (self.data[count..counter], 0..counter - count) |item, index| {
+        pub fn dequeue_count(self: *Self, count: usize) DequeueError!void {
+            if (count > self.counter) {
+                return DequeueError.CannotDequeueAmount;
+            }
+            for (self.data[count..self.counter], 0..self.counter - count) |item, index| {
                 self.data[index] = item;
             }
-            counter = counter - count;
+            self.counter = self.counter - count;
         }
     };
 }
 
 test "queue" {
     var queue = GenericQueue(i32, 10).Create();
+    try std.testing.expectEqual(0, queue.Count());
     try std.testing.expectEqual(0, queue.read_all_values().len);
     try queue.enqueue(10);
     try queue.enqueue(11);
@@ -44,12 +49,12 @@ test "queue" {
     try std.testing.expectEqual(11, all_values[1]);
     try std.testing.expectEqual(12, all_values[2]);
 
-    queue.dequeue_count(2);
+    try queue.dequeue_count(2);
     all_values = queue.read_all_values();
     try std.testing.expectEqual(1, all_values.len);
     try std.testing.expectEqual(12, all_values[0]);
 
-    queue.dequeue_count(1);
+    try queue.dequeue_count(1);
     all_values = queue.read_all_values();
     try std.testing.expectEqual(0, all_values.len);
 
@@ -62,8 +67,9 @@ test "queue" {
     try std.testing.expectEqual(21, all_values[1]);
 }
 
-test "hitting maximum" {
+test "enqueue error" {
     var queue = GenericQueue(i32, 5).Create();
+    try std.testing.expectEqual(0, queue.Count());
     try std.testing.expectEqual(0, queue.read_all_values().len);
     try queue.enqueue(1);
     try queue.enqueue(2);
@@ -75,5 +81,29 @@ test "hitting maximum" {
     const result = queue.enqueue(6);
     try std.testing.expectEqual(EnqueueError.CapacityExceeded, result);
 
-    // this test exists to ensure we well have this handled nicely
+    try queue.dequeue_count(2);
+    try queue.enqueue(10);
+    try queue.enqueue(11);
+
+    const all_values = queue.read_all_values();
+    try std.testing.expectEqual(5, all_values.len);
+    try std.testing.expectEqual(3, all_values[0]);
+    try std.testing.expectEqual(4, all_values[1]);
+    try std.testing.expectEqual(5, all_values[2]);
+    try std.testing.expectEqual(10, all_values[3]);
+    try std.testing.expectEqual(11, all_values[4]);
+}
+
+test "dequeue error" {
+    var queue = GenericQueue(i32, 5).Create();
+    try std.testing.expectEqual(0, queue.Count());
+    try std.testing.expectEqual(0, queue.read_all_values().len);
+    try queue.enqueue(1);
+    try queue.enqueue(2);
+    try queue.enqueue(3);
+    try queue.enqueue(4);
+
+    try queue.dequeue_count(2);
+    const result = queue.dequeue_count(3);
+    try std.testing.expectEqual(DequeueError.CannotDequeueAmount, result);
 }
