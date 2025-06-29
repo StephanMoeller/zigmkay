277c458b6742c261b2c907546190884b27691f1c
Stephan Moeller <stephanryer@hotmail.com>
2025-06-29 16:21:06 +0200

    rearranged tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
-zig test src/zigmkay/generic_queue.zig
+zig test src/zigmkay/generic_queue.test.zig
 zig test src/zigmkay/processor.test.zig
--- src/zigmkay/generic_queue.zig
+++ src/zigmkay/generic_queue.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
-const EnqueueError = error{CapacityExceeded};
-const DequeueError = error{CannotDequeueAmount};
+pub const EnqueueError = error{CapacityExceeded};
+pub const DequeueError = error{CannotDequeueAmount};
 pub fn GenericQueue(comptime T: type, comptime max_capacity: usize) type {
     return struct {
         const Self = @This();
@@ -44,77 +44,4 @@ pub fn GenericQueue(comptime T: type, comptime max_capacity: usize) type {
             self.counter = self.counter - count;
         }
     };
-}
-
-test "queue" {
-    var queue = GenericQueue(i32, 10).Create();
-    try std.testing.expectEqual(0, queue.Count());
-    try std.testing.expectEqual(0, queue.read_all_values().len);
-    try queue.enqueue(10);
-    try queue.enqueue(11);
-    try queue.enqueue(12);
-
-    var all_values = queue.read_all_values();
-    try std.testing.expectEqual(3, all_values.len);
-    try std.testing.expectEqual(10, all_values[0]);
-    try std.testing.expectEqual(11, all_values[1]);
-    try std.testing.expectEqual(12, all_values[2]);
-
-    try queue.dequeue_count(2);
-    all_values = queue.read_all_values();
-    try std.testing.expectEqual(1, all_values.len);
-    try std.testing.expectEqual(12, all_values[0]);
-
-    try queue.dequeue_count(1);
-    all_values = queue.read_all_values();
-    try std.testing.expectEqual(0, all_values.len);
-
-    try queue.enqueue(20);
-    try queue.enqueue(21);
-
-    all_values = queue.read_all_values();
-    try std.testing.expectEqual(2, all_values.len);
-    try std.testing.expectEqual(20, all_values[0]);
-    try std.testing.expectEqual(21, all_values[1]);
-}
-
-test "enqueue error" {
-    var queue = GenericQueue(i32, 5).Create();
-    try std.testing.expectEqual(0, queue.Count());
-    try std.testing.expectEqual(0, queue.read_all_values().len);
-    try queue.enqueue(1);
-    try queue.enqueue(2);
-    try queue.enqueue(3);
-    try queue.enqueue(4);
-    try queue.enqueue(5);
-
-    try std.testing.expectEqual(5, queue.read_all_values().len);
-    const result = queue.enqueue(6);
-    try std.testing.expectEqual(EnqueueError.CapacityExceeded, result);
-
-    try queue.dequeue_count(2);
-    try queue.enqueue(10);
-    try queue.enqueue(11);
-
-    const all_values = queue.read_all_values();
-    try std.testing.expectEqual(5, all_values.len);
-    try std.testing.expectEqual(3, all_values[0]);
-    try std.testing.expectEqual(4, all_values[1]);
-    try std.testing.expectEqual(5, all_values[2]);
-    try std.testing.expectEqual(10, all_values[3]);
-    try std.testing.expectEqual(11, all_values[4]);
-}
-
-test "dequeue error" {
-    var queue = GenericQueue(i32, 5).Create();
-    try std.testing.expectEqual(0, queue.Count());
-    try std.testing.expectEqual(0, queue.read_all_values().len);
-    try queue.enqueue(1);
-    try queue.enqueue(2);
-    try queue.enqueue(3);
-    try queue.enqueue(4);
-
-    try queue.dequeue_count(2);
-    const result = queue.dequeue_count(3);
-    try std.testing.expectEqual(DequeueError.CannotDequeueAmount, result);
 }
