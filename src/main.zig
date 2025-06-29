3329ac21085772c18c0a921b3074de931fc92b52
Stephan Moeller <stephanryer@hotmail.com>
2025-06-29 16:00:50 +0200

    comments changed in main

--- src/main.zig
+++ src/main.zig
@@ -16,17 +16,24 @@ pub fn main() !void {
         // TODO: create scanner and mapping from scanning to keymap positions
         // TODO: create some sort of key state to be fired with usb
 
-        // Read pin states and add keyboard events (what swithes changed state since last tick) to the event queue
-        // Debounce logic will happen inside this scanner
+        // Detect keyboard changes and handle debouncing as part of this
+        // Write to: keyboard_state_change_queue
         try scanner.DetectKeyboardChanges(&keyboard_state_change_queue);
 
         // Read keyboard events and produce output commands eg what hid keycodes should be fired, what layer changes should be applied
         // Tap/Hold timings will be handled inhere.
-        // Note: Only keyboard events that were conclusive are removed at this tick. if a tap/hold key was recently pressed, we may need to wait for more time to pass or other keypresses/releases to happen before we can determine what should happen. Of this reason, it is important that the same queue is continuesly used between loop ticks
+        // Note: Only keyboard events that were conclusive are removed at this tick. if a tap/hold key was recently pressed,
+        // we may need to wait for more time to pass or other keypresses/releases to happen before we can determine what should happen.
+        // Of this reason, it is important that the same queue is continuesly used between loop ticks
+
+        // Read from: board_state_change_queue
+        // Process
+        // Write to: output_command_queue
         try processor.Process(keyboard.KeyCount, keyboard.LayerCount, &keyboard.keymap, &keyboard_state_change_queue, &output_command_queue);
 
         // TODO: Loop through the output commands and execute key strokes and apply layer changes
+
+        // Read from: output_command_queue
+        // Execute using usb helper
     }
 }
-
-
