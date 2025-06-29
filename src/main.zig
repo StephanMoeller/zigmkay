b14a7a58b433d363cee33699cc08cce7feefbb6f
Stephan Moeller <stephanryer@hotmail.com>
2025-06-29 20:58:55 +0200

    moved stuff into zigmkay_main

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,5 @@
-const std = @import("std");
-const zigmkay = @import("zigmkay/bundle.zig");
-const keyboard = @import("wilson26/wilson26.zig");
+const code = @import("zigmkay_main.zig");
 
 pub fn main() !void {
-    const scanner = zigmkay.Scanner{};
-    const processor = zigmkay.Processor{};
-
-    var keyboard_state_change_queue = zigmkay.KeyboardStateChangeQueue.Create();
-    var output_command_queue = zigmkay.OutputCommandQueue.Create();
-
-    while (true) {
-
-        // Changes => keyboard_state_change_queue
-        try scanner.DetectKeyboardChanges(&keyboard_state_change_queue);
-
-        // keyboard_state_change_queue => Process => output_command_queue
-        try processor.Process(keyboard.KeyCount, keyboard.LayerCount, &keyboard.keymap, &keyboard_state_change_queue, &output_command_queue);
-
-        // TODO: Loop through the output commands and execute key strokes and apply layer changes
-
-        // Read from: output_command_queue
-        // Execute using usb helper
-    }
+    try code.main();
 }
