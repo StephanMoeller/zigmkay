9b1282af36c6c39d25bae9a40b17234157fde1b0
Stephan Moeller <stephanryer@hotmail.com>
2025-07-10 22:56:53 +0200

    more mapped keys

--- src/wilson26/wilson26.zig
+++ src/wilson26/wilson26.zig
@@ -9,45 +9,10 @@ pub const keymap = [LayerCount][KeyCount]core.KeyDef{
             .{ //--------------------- 0 ---------------------
     W, R, P, B,             K, L, O, U,
  F, A, S, T, G,             M, N, E, I, Y,
-    X, C, D,                   H, H, H,
+    X, C, D,                   H, COMM, DOT,
           ENTER, ENTER, SPACE, SPACE
     },
 };
-// zig fmt: off
-fn LS(key: core.KeyDef) core.KeyDef { return key; }
-fn LC(key: core.KeyDef) core.KeyDef { return key; }
-fn LG(key: core.KeyDef) core.KeyDef { return key; }
-fn LA(key: core.KeyDef) core.KeyDef { return key; }
-fn RS(key: core.KeyDef) core.KeyDef { return key; }
-fn RC(key: core.KeyDef) core.KeyDef { return key; }
-fn RG(key: core.KeyDef) core.KeyDef { return key; }
-fn RA(key: core.KeyDef) core.KeyDef { return key; }
-fn L1(key: core.KeyDef) core.KeyDef { return key; }
-fn L2(key: core.KeyDef) core.KeyDef { return key; }
-fn L3(key: core.KeyDef) core.KeyDef { return key; }
-// zig fmt: on
-
-fn HL(key: core.KeyDef, layer: u8) core.KeyDef {
-    _ = layer;
-    return key;
-}
-
-const SQ = FromKey(0x15);
-const COM = FromKey(0x15);
-const DO = FromKey(0x15);
-const DASH = FromKey(0x15);
-const DOT = FromKey(0x15);
-const SPC = FromKey(0x15);
-const ENT = FromKey(0x15);
-
-const LCTL = FromKey(0x15);
-const RCTL = FromKey(0x15);
-const LALT = FromKey(0x15);
-const RALT = FromKey(0x15);
-const LSFT = FromKey(0x15);
-const RSFT = FromKey(0x15);
-const LGUI = FromKey(0x15);
-const RGUI = FromKey(0x15);
 
 const A = FromKey(4);
 const B = FromKey(5);
@@ -76,62 +41,26 @@ const X = FromKey(27);
 const Y = FromKey(28);
 const Z = FromKey(29);
 
-const N1 = FromKey(0x15);
-const N2 = FromKey(0x15);
-const N3 = FromKey(0x15);
-const N4 = FromKey(0x15);
-const N5 = FromKey(0x15);
-const N6 = FromKey(0x15);
-const N7 = FromKey(0x15);
-const N8 = FromKey(0x15);
-const N9 = FromKey(0x15);
-const N0 = FromKey(0x15);
+const ENTER = FromKey(0x0028);
+const SPACE = FromKey(0x002C);
+const COMM = FromKey(0x0036);
+const DOT = FromKey(0x0037);
+
+const N1 = FromKey(0x001E);
+const N2 = FromKey(0x001F);
+const N3 = FromKey(0x0020);
+const N4 = FromKey(0x0021);
+const N5 = FromKey(0x0022);
+const N6 = FromKey(0x0023);
+const N7 = FromKey(0x0024);
+const N8 = FromKey(0x0025);
+const N9 = FromKey(0x0026);
+const N0 = FromKey(0x0027);
 
 const Ae = FromKey(0x15);
 const Oe = FromKey(0x15);
 const Aa = FromKey(0x15);
 
-const @"!" = FromKey_LSFT(N1.keycode);
-const @"<" = FromKey_LSFT(N1.keycode);
-const @"=" = FromKey_LSFT(N1.keycode);
-const @">" = FromKey_LSFT(N1.keycode);
-const @"%" = FromKey_LSFT(N1.keycode);
-const @"/" = FromKey_LSFT(N1.keycode);
-const home = FromKey_LSFT(N1.keycode);
-const @"↑" = FromKey_LSFT(N1.keycode);
-const end = FromKey_LSFT(N1.keycode);
-const @"?" = FromKey_LSFT(N1.keycode);
-const @"@" = FromKey_LSFT(N1.keycode);
-const @"{" = FromKey_LSFT(N1.keycode);
-const @"(" = FromKey_LSFT(N1.keycode);
-const @")" = FromKey_LSFT(N1.keycode);
-const @"}" = FromKey_LSFT(N1.keycode);
-const pgup = FromKey_LSFT(N1.keycode);
-const @"←" = FromKey_LSFT(N1.keycode);
-const @"↓" = FromKey_LSFT(N1.keycode);
-const @"→" = FromKey_LSFT(N1.keycode);
-const pgdn = FromKey_LSFT(N1.keycode);
-const @"\\" = FromKey_LSFT(N1.keycode);
-const @"#" = FromKey_LSFT(N1.keycode);
-const @"[" = FromKey_LSFT(N1.keycode);
-const @"]" = FromKey_LSFT(N1.keycode);
-const @"&" = FromKey_LSFT(N1.keycode);
-const @"|" = FromKey_LSFT(N1.keycode);
-const tab = FromKey_LSFT(N1.keycode);
-const DQ = FromKey_LSFT(N1.keycode);
-const ESC = FromKey_LSFT(N1.keycode);
-const @"." = FromKey_LSFT(N1.keycode);
-const @"-" = FromKey_LSFT(N1.keycode);
-const ENTER = FromKey_LSFT(N1.keycode);
-const SPACE = FromKey_LSFT(N1.keycode);
-const ____ = FromKey(0);
-
-fn FromKey_LSFT(keycode: u8) core.KeyDef {
-    return core.KeyDef{ .keycode = keycode };
-}
-fn FromKey_RAlt(keycode: u8) core.KeyDef {
-    return core.KeyDef{ .keycode = keycode };
-}
 fn FromKey(keycode: u8) core.KeyDef {
     return core.KeyDef{ .keycode = keycode };
 }
