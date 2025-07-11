260c31162a8ef3d92bdbbaf24e3f044425f9ce28
Stephan Moeller <stephanryer@hotmail.com>
2025-07-11 15:29:31 +0200

    comment

--- src/zigmkay/processing.zig
+++ src/zigmkay/processing.zig
@@ -31,24 +31,21 @@ const Processor = struct {
         while (input.Count() > 0) {
             const next_event = try input.dequeue();
             const current_layer_index: usize = 0;
-            const key_def = keymap[current_layer_index][next_event.key_index];
             if (next_event.pressed == 1) {
-                if (key_def.tap_keycode == 4) {
-                    modifiers.left_shift = true;
-                    try output_queue.enqueue(core.OutputCommand{ .ModifiersChanged = modifiers });
-                } else {
+                const key_def = keymap[current_layer_index][next_event.key_index];
+                if (key_def.tap_keycode != 0) {
                     try output_queue.enqueue(core.OutputCommand{ .KeyCodePress = key_def.tap_keycode });
                 }
                 //modifiers.left_shift = true;
                 //try output_queue.enqueue(core.OutputCommand{ .ModifiersChanged = modifiers });
             } else {
-                if (key_def.tap_keycode == 4) {
-                    modifiers.left_shift = false;
-                    try output_queue.enqueue(core.OutputCommand{ .ModifiersChanged = modifiers });
-                } else {
-                    try output_queue.enqueue(core.OutputCommand{ .KeyCodeRelease = key_def.tap_keycode });
+                // TODO:
+                // key_def should not be read from the layout but be the exact key that was pressed to ensure a layer switch
+                // between press and release will still trigger releasing of the original key and not the one on the new layer
+                const key_def = keymap[current_layer_index][next_event.key_index];
+                if (key_def.tap_keycode != 0) {
+                    try output_queue.enqueue(core.OutputCommand{ .KeyCodePress = key_def.tap_keycode });
                 }
-
                 //modifiers.left_shift = false;
                 //try output_queue.enqueue(core.OutputCommand{ .ModifiersChanged = modifiers });
             }
