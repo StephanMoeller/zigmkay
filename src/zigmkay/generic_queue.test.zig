9a566d659e1d55b99c2423981dc55c581d75e0f6
Stephan Moeller <stephanryer@hotmail.com>
2025-07-11 14:27:23 +0200

    tests fixed

--- src/zigmkay/generic_queue.test.zig
+++ src/zigmkay/generic_queue.test.zig
@@ -3,75 +3,72 @@ const q = @import("generic_queue.zig");
 test "queue" {
     var queue = q.GenericQueue(i32, 10).Create();
     try std.testing.expectEqual(0, queue.Count());
-    try std.testing.expectEqual(0, queue.read_all_values().len);
     try queue.enqueue(10);
     try queue.enqueue(11);
     try queue.enqueue(12);
 
-    var all_values = queue.read_all_values();
     try std.testing.expectEqual(3, queue.Count());
-    try std.testing.expectEqual(3, all_values.len);
-    try std.testing.expectEqual(10, all_values[0]);
-    try std.testing.expectEqual(11, all_values[1]);
-    try std.testing.expectEqual(12, all_values[2]);
-
-    try queue.dequeue_count(2);
-    all_values = queue.read_all_values();
-    try std.testing.expectEqual(1, all_values.len);
+    try std.testing.expectEqual(10, queue.dequeue());
+    try std.testing.expectEqual(11, queue.dequeue());
     try std.testing.expectEqual(1, queue.Count());
-    try std.testing.expectEqual(12, all_values[0]);
-
-    _ = try queue.dequeue();
-    all_values = queue.read_all_values();
-    try std.testing.expectEqual(0, all_values.len);
+    try std.testing.expectEqual(12, queue.dequeue());
     try std.testing.expectEqual(0, queue.Count());
 
     try queue.enqueue(20);
     try queue.enqueue(21);
 
-    all_values = queue.read_all_values();
-    try std.testing.expectEqual(2, all_values.len);
-    try std.testing.expectEqual(20, all_values[0]);
-    try std.testing.expectEqual(21, all_values[1]);
+    try std.testing.expectEqual(2, queue.Count());
+
+    try std.testing.expectEqual(20, queue.dequeue());
+    try std.testing.expectEqual(21, queue.dequeue());
+
+    try std.testing.expectEqual(0, queue.Count());
 }
 
 test "enqueue error" {
     var queue = q.GenericQueue(i32, 5).Create();
     try std.testing.expectEqual(0, queue.Count());
-    try std.testing.expectEqual(0, queue.read_all_values().len);
-    try queue.enqueue(1);
-    try queue.enqueue(2);
-    try queue.enqueue(3);
-    try queue.enqueue(4);
-    try queue.enqueue(5);
+    _ = try queue.enqueue(1);
+    _ = try queue.enqueue(2);
+    _ = try queue.enqueue(3);
+    _ = try queue.enqueue(4);
+    _ = try queue.enqueue(5);
 
-    try std.testing.expectEqual(5, queue.read_all_values().len);
+    try std.testing.expectEqual(5, queue.Count());
     const result = queue.enqueue(6);
     try std.testing.expectEqual(q.EnqueueError.CapacityExceeded, result);
 
-    try queue.dequeue_count(2);
+    try std.testing.expectEqual(1, queue.dequeue());
+    try std.testing.expectEqual(2, queue.dequeue());
+    try std.testing.expectEqual(3, queue.Count());
     try queue.enqueue(10);
     try queue.enqueue(11);
 
-    const all_values = queue.read_all_values();
-    try std.testing.expectEqual(5, all_values.len);
-    try std.testing.expectEqual(3, all_values[0]);
-    try std.testing.expectEqual(4, all_values[1]);
-    try std.testing.expectEqual(5, all_values[2]);
-    try std.testing.expectEqual(10, all_values[3]);
-    try std.testing.expectEqual(11, all_values[4]);
+    try std.testing.expectEqual(5, queue.Count());
+
+    try std.testing.expectEqual(3, queue.dequeue());
+    try std.testing.expectEqual(4, queue.dequeue());
+    try std.testing.expectEqual(5, queue.dequeue());
+    try std.testing.expectEqual(10, queue.dequeue());
+    try std.testing.expectEqual(11, queue.dequeue());
 }
 
 test "dequeue error" {
     var queue = q.GenericQueue(i32, 5).Create();
     try std.testing.expectEqual(0, queue.Count());
-    try std.testing.expectEqual(0, queue.read_all_values().len);
     try queue.enqueue(1);
     try queue.enqueue(2);
     try queue.enqueue(3);
     try queue.enqueue(4);
 
-    try queue.dequeue_count(2);
-    const result = queue.dequeue_count(3);
+    _ = try queue.dequeue();
+    _ = try queue.dequeue();
+    _ = try queue.dequeue();
+    _ = try queue.dequeue();
+
+    const result = queue.dequeue();
     try std.testing.expectEqual(q.DequeueError.NoElements, result);
+
+    try queue.enqueue(5);
+    try std.testing.expectEqual(5, queue.dequeue());
 }
--- src/zigmkay/generic_queue.zig
+++ src/zigmkay/generic_queue.zig
@@ -6,48 +6,32 @@ pub fn GenericQueue(comptime T: type, comptime max_capacity: usize) type {
     return struct {
         const Self = @This();
         data: [max_capacity]T,
-        counter: usize = 0,
+        size: usize = 0,
         pub fn Create() Self {
             return Self{ .data = [1]T{undefined} ** max_capacity };
         }
         pub fn Count(self: *Self) usize {
-            return self.counter;
+            return self.size;
         }
         pub fn enqueue(self: *Self, element: T) EnqueueError!void {
-            if (self.counter == max_capacity) {
+            if (self.size == max_capacity) {
                 return EnqueueError.CapacityExceeded;
             }
-            self.data[self.counter] = element;
-            self.counter += 1;
+            self.data[self.size] = element;
+            self.size += 1;
         }
-        pub fn read_all_values(self: *Self) []const T {
-            return self.data[0..self.counter];
-        }
-        pub fn peek(self: *Self) DequeueError!T {
-            if (self.counter == 0) {
+        pub fn dequeue(self: *Self) DequeueError!T {
+            if (self.size == 0) {
                 return DequeueError.NoElements;
             }
             const head_element = self.data[0];
-            return head_element;
-        }
-        pub fn dequeue(self: *Self) DequeueError!T {
-            const head_element = try self.peek();
 
             // todo: don't do this shifting of all values. Use a better queue implementation instead
-            for (self.data[1..self.counter], 0..self.counter - 1) |item, index| {
+            for (self.data[1..self.size], 0..self.size - 1) |item, index| {
                 self.data[index] = item;
             }
-            self.counter = self.counter - 1;
+            self.size = self.size - 1;
             return head_element;
-        }
-        pub fn dequeue_count(self: *Self, count: usize) DequeueError!void {
-            if (count > self.counter) {
-                return DequeueError.NoElements;
-            }
-            for (self.data[count..self.counter], 0..self.counter - count) |item, index| {
-                self.data[index] = item;
-            }
-            self.counter = self.counter - count;
         }
     };
 }
