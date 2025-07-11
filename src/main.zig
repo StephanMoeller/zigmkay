889114ac893dfae0f97b544435476d420a60e2a1
Stephan Moeller <stephanryer@hotmail.com>
2025-07-11 11:38:26 +0200

    using single main now

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,22 @@
-//const code = @import("poc_main.zig");
-const code = @import("zigmkay_main.zig");
+const std = @import("std");
+const zigmkay = @import("zigmkay/zigmkay.zig");
+const keyboard = @import("wilson26/wilson26.zig");
+const rp2xxx = @import("microzig").hal;
+
 pub fn main() !void {
-    try code.main();
+
+    // data queues
+    var keyboard_state_change_queue = zigmkay.core.KeyboardStateChangeQueue.Create();
+    var output_command_queue = zigmkay.core.OutputCommandQueue.Create();
+
+    // logic
+    const scanner = zigmkay.CreateScanner();
+    const processor = zigmkay.CreateProcessor();
+    const usb_command_executor = zigmkay.CreateAndInitUsbCommandExecutor();
+
+    while (true) {
+        try scanner.DetectKeyboardChanges(&keyboard_state_change_queue);
+        try processor.Process(keyboard.KeyCount, keyboard.LayerCount, &keyboard.keymap, &keyboard_state_change_queue, &output_command_queue);
+        usb_command_executor.HouseKeepAndProcessCommands(&output_command_queue);
+    }
 }
