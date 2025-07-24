81265798dd82ee39477ea8a32c79de348c1e2811
Stephan Moeller <stephanryer@hotmail.com>
2025-07-24 16:30:38 +0200

    more keymap

--- src/zigmkay/core.zig
+++ src/zigmkay/core.zig
@@ -41,7 +41,7 @@ pub const KeyDef = union(enum) {
     pub fn MO(layer: LayerIndex) KeyDef {
         return KeyDef{ .hold_only = .{ .hold_layer = layer } };
     }
-    pub fn LT(layer: LayerIndex, tap_keycode: u8, tap_modifiers: Modifiers, tapping_term_ms: u16) KeyDef {
+    pub fn LT(layer: LayerIndex, tap_keycode: u8, tap_modifiers: Modifiers, tapping_term_ms: TappingTermType) KeyDef {
         return KeyDef{ .tap_hold = .{
             .tap = .{ .tap_keycode = tap_keycode, .tap_modifiers = tap_modifiers },
             .hold = .{ .hold_layer = layer },
@@ -49,7 +49,7 @@ pub const KeyDef = union(enum) {
             .retro_tapping = false,
         } };
     }
-    pub fn MT(tap: TapDef, hold_mods: Modifiers, tapping_term_ms: u64) KeyDef {
+    pub fn MT(tap: TapDef, hold_mods: Modifiers, tapping_term_ms: TappingTermType) KeyDef {
         return KeyDef{ .tap_hold = .{
             .tap = tap,
             .hold = HoldDef{ .hold_modifiers = hold_mods },
--- src/zilpzalp/keymap.zig
+++ src/zilpzalp/keymap.zig
@@ -3,33 +3,65 @@ const std = @import("std");
 const core = @import("../zigmkay/core.zig");
 
 pub const key_count = 28;
+
+// zig fmt: off
+
 pub const keymap = [_][key_count]core.KeyDef{
-    // zig fmt: off
-            .{ //--------------------- 0 ---------------------
-    T(kc.W),  T(kc.R), LG(kc.P),  T(kc.B),               T(kc.K), LG(kc.L),  T(kc.O),     T(kc.U),
+// layer 0
+    .{ 
+              T(kc.W),  T(kc.R), LG(kc.P), T(kc.B),      T(kc.K), LG(kc.L),  T(kc.O),     T(kc.U),
     T(kc.F), LA(kc.A), LC(kc.S), LS(kc.T), T(kc.G),      T(kc.M), LS(kc.N), LC(kc.E),    LA(kc.I),  T(kc.Y),
-              T(kc.X),  T(kc.C),  T(kc.D),                         T(kc.H),  T(kc.COMMA), T(kc.DOT),
-          T(kc.ENTER), core.KeyDef.NONE(), core.KeyDef.NONE(), T(kc.SPACE)
+              T(kc.X),  T(kc.C),  T(kc.D),                         T(kc.H), T(kc.COMMA),  T(kc.DOT),
+          LT(2, kc.ENTER), core.KeyDef.NONE(), core.KeyDef.NONE(), LT(1, kc.SPACE)
+    },
+
+    // Layer 1: arrows keys and symbols
+    .{ 
+             _______, _______, _______, _______,             _______, _______,     T(kc.UP),    _______,
+    _______, _______, _______, _______, _______,             _______, T(kc.LEFT), T(kc.DOWN), T(kc.RIGHT), _______,
+             _______, _______, _______,                               _______,     _______,     _______,
+                       LT(1, kc.SPACE), _______,    _______, _______
+    }, 
+
+    // Layer 2: numbers
+    .{ 
+             _______, _______, _______, _______,             _______, T(kc.N7), T(kc.N8), T(kc.N9),
+    _______, _______, _______, _______, _______,             _______, T(kc.N4), T(kc.N5), T(kc.N6), T(kc.N6),
+             _______, _______, _______,                               T(kc.N1), T(kc.N2), T(kc.N3),
+                               _______, _______,    _______, T(kc.N0)
+    },
+
+    // Layer 3: backspace
+    .{ 
+             _______, _______, _______, _______,             _______, T(kc.SPACE), T(kc.SPACE), T(kc.SPACE),
+    _______, _______, _______, _______, _______,             _______, T(kc_bs),    T(kc_bs),    T(kc_bs),    _______,
+             _______, _______, _______,                               T(kc_del),   T(kc_del),   T(kc_del),
+                       _______, _______,    _______, T(kc.N0)
     }
+
 };
-
+// zig fmt: on
 pub const dimensions = core.KeymapDimensions{ .key_count = key_count, .layer_count = keymap.len };
-
-fn T(keycode: u8) core.KeyDef{
-    return core.KeyDef.TAP(keycode);
-}
-
+const _______ = core.KeyDef.TRANSPARENT();
+pub const kc_bs = kc.BACKSPACE;
+pub const kc_del = kc.DELETE;
 const tapping_term_us = 250 * 1000; // 250 ms = 250,000 micro seconds
 
+fn LT(layer_index: core.LayerIndex, keycode: u8) core.KeyDef {
+    return core.KeyDef.LT(layer_index, keycode, .{}, tapping_term_us);
+}
+fn T(keycode: u8) core.KeyDef {
+    return core.KeyDef.TAP(keycode);
+}
 fn LG(keycode: u8) core.KeyDef {
-    return core.KeyDef.MT(core.TapDef{.tap_keycode = keycode}, .{.left_gui = true}, tapping_term_us );
+    return core.KeyDef.MT(core.TapDef{ .tap_keycode = keycode }, .{ .left_gui = true }, tapping_term_us);
 }
 fn LC(keycode: u8) core.KeyDef {
-    return core.KeyDef.MT(core.TapDef{.tap_keycode = keycode}, .{.left_ctrl = true}, tapping_term_us );
+    return core.KeyDef.MT(core.TapDef{ .tap_keycode = keycode }, .{ .left_ctrl = true }, tapping_term_us);
 }
 fn LA(keycode: u8) core.KeyDef {
-    return core.KeyDef.MT(core.TapDef{.tap_keycode = keycode}, .{.left_alt = true}, tapping_term_us );
+    return core.KeyDef.MT(core.TapDef{ .tap_keycode = keycode }, .{ .left_alt = true }, tapping_term_us);
 }
 fn LS(keycode: u8) core.KeyDef {
-    return core.KeyDef.MT(core.TapDef{.tap_keycode = keycode}, .{.left_shift = true}, tapping_term_us );
+    return core.KeyDef.MT(core.TapDef{ .tap_keycode = keycode }, .{ .left_shift = true }, tapping_term_us);
 }
