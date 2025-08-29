ef1a1b681a63d93b6ff9672a834d939b0ced5242
Stephan Moeller <stephanryer@hotmail.com>
2025-08-29 13:48:16 +0200

    test commit

--- src/keyboards/miketypeson/keymap.zig
+++ src/keyboards/miketypeson/keymap.zig
@@ -59,7 +59,7 @@ pub const keymap = [_][key_count]core.KeyDef{
              _______, _______, _______, _______,             _______,   T(dk.N1), T(dk.N2), T(dk.N3),
                             _______,                         LT(1, dk.N0)
     },
-    .{ 
+    .{  
     PrintStats,   T(us.F7),   T(us.F8),   T(us.F9), T(us.F10),            T(dk.TILD), T(us.SPACE), T(us.SPACE), T(us.SPACE), T(dk.GRV),
     T(us.BOOT), ALT(us.F4), CTL(us.F5), SFT(us.F6), T(us.F11),             T(dk.DLR),  SFT(us.BS),  CTL(us.BS),  ALT(us.BS), _______,
                   T(us.F1),   T(us.F2),   T(us.F3), T(us.F12),               T(dk.CIRC),   T(us.DEL),   T(us.DEL),   T(us.DEL),
