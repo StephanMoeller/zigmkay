7d11b91e3bb3335e28ccc2402468db184efdb28b
Stephan Moeller <stephanryer@hotmail.com>
2025-09-15 22:51:19 +0200

    work in progress

--- src/zigmkay/matrix_scanning.zig
+++ src/zigmkay/matrix_scanning.zig
@@ -21,79 +21,55 @@ pub fn CreateMatrixScannerType(
     comptime pins_to_keys_mapping: [keymap_dimensions.key_count]?[2]usize,
     comptime settings: ScannerSettings,
 ) type {
-    comptime var pins_with_indexes: [keymap_dimensions.key_count]PinAndIndex = [1]PinAndIndex{undefined} ** keymap_dimensions.key_count;
-    // Build a new array where the actual positon of the mappings does not matter
-    // Instead the new array will contain PinAndIndex strucs, where the original array index will now
-    // be stored as key_index in the new array
-    for (pins_to_keys_mapping, 0..) |pin_index_pair_or_null, key_index| {
-        if (pin_index_pair_or_null) |pin_index_pair| {
-            pins_with_indexes[key_index] = PinAndIndex{
-                .col_index = pin_index_pair[0],
-                .row_index = pin_index_pair[1],
-                .key_index = key_index,
-            };
+    comptime var row_col_to_keyindex: [pin_cols.len][pin_rows.len]?core.KeyIndex = @splat(@splat(null));
+    for (pins_to_keys_mapping, 0..) |pins_or_null, key_index| {
+        if (pins_or_null) |pins| {
+            const col_idx = pins[0];
+            const row_idx = pins[1];
+            row_col_to_keyindex[col_idx][row_idx] = key_index;
         }
     }
-
-    // Just bubble sort the thing - its a small array and happening at comptime
-    //
-    var i: usize = 0;
-    while (i < pins_with_indexes.len) {
-        var j: usize = i;
-        while (j > 0 and pins_with_indexes[j].col_index < pins_with_indexes[j - 1].col_index) {
-            const temp = pins_with_indexes[j - 1];
-            pins_with_indexes[j - 1] = pins_with_indexes[j];
-            pins_with_indexes[j] = temp;
-            j -= 1;
-        }
-        i += 1;
-    }
-
     return struct {
         // current_states should be a packed struct
         var current_states: [pins_to_keys_mapping.len]bool = [1]bool{false} ** (pins_to_keys_mapping.len);
         var current_states_last_changed: [pins_to_keys_mapping.len]u64 = [1]u64{0} ** (pins_to_keys_mapping.len);
 
+        // map col+row coordinates to keymap positions
+
         const Self = @This();
         pub fn DetectKeyboardChanges(_: *const Self, output_queue: *core.MatrixStateChangeQueue, current_time: core.TimeSinceBoot) !void {
-            var previous_col_index: usize = 255;
-            for (pins_with_indexes, 0..) |pins_and_index, array_idx| {
-                var col = pin_cols[pins_and_index.col_index];
-                var row = pin_rows[pins_and_index.row_index];
-                // Only sleep if a new col is present now
-                if (previous_col_index != pins_and_index.col_index) {
-                    col.put(1);
-                    if (array_idx > 0) {
-                        pin_cols[previous_col_index].put(0);
-                    }
-                    time.sleep_us(settings.pin_raise_wait_us);
-                }
-                previous_col_index = pins_and_index.col_index;
+            for (pin_cols, 0..) |col, col_idx| {
+                col.put(1);
+                time.sleep_us(settings.pin_raise_wait_us);
 
-                const read_value = row.read();
-                const pressed = read_value == 1;
-                if (pressed != current_states[pins_and_index.key_index]) {
-                    // DEBOUNCE HANDLING
-                    // This state has changed. If this happened last time very recently, this could be a debounce.
-                    // Then let it be for now. In a future tick this will be picked up and handled correctly if it is still at the current state by then.
-                    const last_changed_time = current_states_last_changed[pins_and_index.key_index];
+                for (pin_rows, 0..) |row, row_idx| {
+                    // find the key index for this combination
+                    const key_index_or_null = row_col_to_keyindex[col_idx][row_idx];
+                    if (key_index_or_null) |key_index| {
+                        const pressed = row.read() == 1;
 
-                    if (current_time.time_since_boot_us - last_changed_time > settings.debounce.ms * 1000) {
-                        current_states[pins_and_index.key_index] = pressed;
-                        current_states_last_changed[pins_and_index.key_index] = current_time.time_since_boot_us;
+                        if (pressed != current_states[key_index]) {
+                            // DEBOUNCE HANDLING
+                            // This state has changed. If this happened last time very recently, this could be a debounce.
+                            // Then let it be for now. In a future tick this will be picked up and handled correctly if it is still at the current state by then.
+                            const last_changed_time = current_states_last_changed[key_index];
 
-                        const key_index_with_type: core.KeyIndex = @intCast(pins_and_index.key_index);
-                        try output_queue.enqueue(.{ .pressed = pressed, .key_index = key_index_with_type, .time = current_time });
-                        //p.led_red.put(read_value);
-                        //p.led_green.put(1 - read_value);
-                        //p.led_blue.put(1);
+                            if (current_time.time_since_boot_us - last_changed_time > settings.debounce.ms * 1000) {
+                                current_states[key_index] = pressed;
+                                current_states_last_changed[key_index] = current_time.time_since_boot_us;
+
+                                const key_index_with_type: core.KeyIndex = @intCast(key_index);
+                                try output_queue.enqueue(.{ .pressed = pressed, .key_index = key_index_with_type, .time = current_time });
+                                //p.led_red.put(read_value);
+                                //p.led_green.put(1 - read_value);
+                                //p.led_blue.put(1);
+                            }
+                        }
                     }
                 }
+
+                col.put(0);
             }
-
-            // now turn off the last col
-            pin_cols[pin_cols.len - 1].put(0);
-
             // zig fmt: off
      }
     };
