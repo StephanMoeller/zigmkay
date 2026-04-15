20f5a179f6b2ba0fd136cc27f5bf5de033cda70b
Stephan (Rollercole) Møller <stephanryer@hotmail.com>
2026-04-15 20:35:18 +0200

    removed unused stuff from root.zig

--- src/root.zig
+++ src/root.zig
@@ -1,11 +1,1 @@
-pub const keycodes = struct {
-    pub const us = @import("keycodes/us.zig");
-    pub const dk = @import("keycodes/dk.zig");
-    pub const us_intl= @import("keycodes/us_intl.zig");
-};
-
-pub const microzig = @import("microzig");
-pub const rp2xxx = microzig.hal;
-pub const time = rp2xxx.time;
-
 pub const zigmkay = @import("zigmkay/zigmkay.zig");
